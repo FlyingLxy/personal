79f8acd80e04de8e48c74b043e539e21dcf97578
郎啸宇 <ui_fox@163.com>
2016-08-15 12:30:31 +0800

    持久化个人简介

--- client/actions/personalAction.js
+++ client/actions/personalAction.js
@@ -1,8 +1,30 @@
-import {updateStep} from '../constants/personalConst.js';
+import {updateStep,resPersonalInfo} from '../constants/personalConst.js';
+import $ from 'jquery';
 
 export const updateStepAction = (step) => {
     return {
         type: updateStep,
         step
+    }
+}
+export const resPersonalAction = (info) => {
+    return {
+        type: resPersonalInfo,
+        info
+    }
+}
+export const getPersonalAction = () => {
+    return (dispatch,getState) => {
+        $.ajax('/api/personal/info',{
+            type: 'GET',
+            success: data => {
+                if (data.msg === 'ok') {
+                    dispatch(resPersonalAction(data.result));
+                }
+            },
+            error: err => {
+                console.log(err);
+            }
+        })
     }
 }
--- server/config/local.env.js
+++ server/config/local.env.js
@@ -4,10 +4,10 @@
 const env = {
     //MODE: 'development',
     MODE: 'production',
-    PORT: 3000,
+    PORT: 80,
     //HOST: '192.168.199.210',
-    HOST: 'http://172.16.33.49',
-    LOCALHOST: 'http://172.16.33.49:3000'
+    HOST: 'http://172.16.33.29',
+    LOCALHOST: 'http://172.16.33.29:3000'
 }
 
 process.env.NODE_ENV = env.MODE;
